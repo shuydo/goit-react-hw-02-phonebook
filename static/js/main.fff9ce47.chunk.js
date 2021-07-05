9e1df879d40bbc601a5bbdea0221ce19b9cf03e8
Shuydo <shuydo12@gmail.com>
2021-07-06 02:12:46 +0300

    Updates

---
+++ static/js/main.fff9ce47.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),o=c.n(n),r=c(3),s=c.n(r),i=c(0);var a=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("p",{children:"Hello"})})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.fff9ce47.chunk.js.map
