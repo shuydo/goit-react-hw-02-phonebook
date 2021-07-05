59c272ad3b740bc89af3a9be8a29073d3730deb6
Shuydo <shuydo12@gmail.com>
2021-07-06 02:11:03 +0300

    1st com after tune

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg' Shu;
+// import './App.css' Shu;
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
